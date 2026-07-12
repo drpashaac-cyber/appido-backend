c3ee1167ba35f8857c6776bc8360300af6e2f978
drpashaac-cyber <drpasha.ac@gmail.com>
2026-07-12 22:14:20 +0330

    fix: register TelegramWebhookController in module

--- services/core-api/src/telegram/telegram.module.ts
+++ services/core-api/src/telegram/telegram.module.ts
@@ -2,12 +2,11 @@ import { Module } from "@nestjs/common";
 import { QueueModule } from "../queue/queue.module";
 import { TelegramService } from "./telegram.service";
 import { TelegramController } from "./telegram.controller";
-// import { TelegramWebhookController } from "./telegram-webhook.controller"; // <--- کامنت شد
+import { TelegramWebhookController } from "./telegram-webhook.controller";
 import { AuthModule } from "../auth/auth.module";
-
 @Module({
   imports: [AuthModule, QueueModule],
-  controllers: [TelegramController], // <--- TelegramWebhookController حذف شد
+  controllers: [TelegramController, TelegramWebhookController],
   providers: [TelegramService],
 })
 export class TelegramModule {}
